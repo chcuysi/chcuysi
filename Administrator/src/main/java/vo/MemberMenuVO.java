028f0c1b7a036b8843f81a4ff51c09a16a8d0bdb
dyne <cloudburst71@gmail.com>
2023-05-09 11:42:29 +0900

    member select 구현

---
+++ Administrator/src/main/java/vo/MemberMenuVO.java
@@ -0,0 +1,65 @@
+package vo;
+
+public class MemberMenuVO {
+	
+    private int mnum;
+    private String mid;
+    private String maddr;
+    private String mdaddr;
+    private String mtel;
+    private String memail;
+    private String mdate;
+    private String mout;
+    
+	public int getMnum() {
+		return mnum;
+	}
+	public void setMnum(int mnum) {
+		this.mnum = mnum;
+	}
+	public String getMid() {
+		return mid;
+	}
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+	public String getMaddr() {
+		return maddr;
+	}
+	public void setMaddr(String maddr) {
+		this.maddr = maddr;
+	}
+	public String getMdaddr() {
+		return mdaddr;
+	}
+	public void setMdaddr(String mdaddr) {
+		this.mdaddr = mdaddr;
+	}
+	public String getMtel() {
+		return mtel;
+	}
+	public void setMtel(String mtel) {
+		this.mtel = mtel;
+	}
+	public String getMemail() {
+		return memail;
+	}
+	public void setMemail(String memail) {
+		this.memail = memail;
+	}
+	public String getMdate() {
+		return mdate;
+	}
+	public void setMdate(String mdate) {
+		this.mdate = mdate;
+	}
+	public String getMout() {
+		return mout;
+	}
+	public void setMout(String mout) {
+		this.mout = mout;
+	}
+
+    
+    
+}
---
+++ Administrator/src/main/java/vo/RefundMenuVO.java
@@ -0,0 +1,49 @@
+package vo;
+
+public class RefundMenuVO {
+	
+    private String dnum;
+    private String onum;
+    private String daddr;
+    private String dok;
+    private String ddate;
+    private String rok;
+    
+	public String getDnum() {
+		return dnum;
+	}
+	public void setDnum(String dnum) {
+		this.dnum = dnum;
+	}
+	public String getOnum() {
+		return onum;
+	}
+	public void setOnum(String onum) {
+		this.onum = onum;
+	}
+	public String getDaddr() {
+		return daddr;
+	}
+	public void setDaddr(String daddr) {
+		this.daddr = daddr;
+	}
+	public String getDok() {
+		return dok;
+	}
+	public void setDok(String dok) {
+		this.dok = dok;
+	}
+	public String getDdate() {
+		return ddate;
+	}
+	public void setDdate(String ddate) {
+		this.ddate = ddate;
+	}
+	public String getRok() {
+		return rok;
+	}
+	public void setRok(String rok) {
+		this.rok = rok;
+	}
+    
+}
